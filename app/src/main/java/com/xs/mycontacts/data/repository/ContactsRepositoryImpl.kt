522bbbf8642a7fb15d66a8f2a2e3a93bcfc657c6
Гусейн <huseinsadigov@gmail.com>
2026-05-13 15:51:31 +0300

    feat: implement contact fetching using ContentProvider and add permissions

--- app/src/main/java/com/xs/mycontacts/data/repository/ContactsRepositoryImpl.kt
+++ app/src/main/java/com/xs/mycontacts/data/repository/ContactsRepositoryImpl.kt
@@ -1,17 +1,49 @@
 package com.xs.mycontacts.data.repository
 
+import android.content.Context
+import android.provider.ContactsContract
 import com.xs.mycontacts.domain.model.Contact
 import com.xs.mycontacts.domain.repository.ContactsRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class ContactsRepositoryImpl @Inject constructor() : ContactsRepository{
+class ContactsRepositoryImpl @Inject constructor(
+    @ApplicationContext
+    private val context: Context
+) : ContactsRepository{
+
     override fun getContacts(): Flow<List<Contact>> = flow {
-        val forTest = listOf(Contact("1", "Torvalds","+79531566435"),
-            Contact("2", "Tenenbaum","+79817046797"),
-            Contact("3", "Torvalds","+79531566435")
+        val contactList = mutableListOf<Contact>()
+
+        val contentResolver = context.contentResolver
+        val uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+
+        val projection = arrayOf(
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER
         )
-        emit(forTest)
-    }
+
+        val cursor = contentResolver.query(uri, projection, null, null, null)
+
+        cursor?.use {
+            val idIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)
+            val nameIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+            val numberIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+
+            while (it.moveToNext()) {
+                val id = if (idIndex >= 0) it.getString(idIndex) else ""
+                val name = it.getString(nameIndex).takeIf { nameIndex >= 0 } ?: "Имени нет"
+                val number = it.getString(numberIndex).takeIf { numberIndex >= 0 } ?: ""
+
+                contactList.add(Contact(id = id, name = name, phoneNumber = number))
+            }
+        }
+    emit(contactList)
+
+    }.flowOn(Dispatchers.IO)
 }
